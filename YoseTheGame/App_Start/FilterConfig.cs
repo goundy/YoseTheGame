5ed025d646fe987c6d6f38116cc33f5b4785c9fa
djreeboot <bchampagne@victrix.ca>
2014-06-02 14:54:40 -0400

    Initial

---
+++ YoseTheGame/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YoseTheGame
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
