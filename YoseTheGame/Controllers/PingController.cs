adb7cb3d610e6f2e7c10f6e467b7d8d4b9642e6e
djreeboot <bchampagne@victrix.ca>
2014-06-02 16:09:06 -0400

    Yo!

---
+++ YoseTheGame/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YoseTheGame.Controllers
+{
+    public class PingController : Controller
+    {
+        //
+        // GET: /Ping/
+
+        public ActionResult Index()
+        {
+            return Json(new { alive = true }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
