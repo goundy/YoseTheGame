04bf170e86f7046dddbfa9ffb672c603980f9e5e
Eric Mignot <eric.mignot@gmail.com>
2014-06-03 10:12:09 -0400

    Add PingControllerTest

---
+++ YoseTheGameTests/PingControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+using NUnit.Framework;
+using YoseTheGame.Controllers;
+
+
+namespace YoseTheGameTests
+{
+    [TestFixture]
+    public class PingControllerTest
+    {
+        private ActionResult _result;
+
+        [SetUp]
+        public void ThisController()
+        {
+            var controller = new PingController();
+            _result = controller.Index();
+        }
+
+        [Test]
+        public void ReturnsJson()
+        {            
+            Assert.That(_result, Is.InstanceOf<JsonResult>());
+        }
+
+        [Test]
+        public void ReturnsTheExpectedJson()
+        {
+            var json = (JsonResult) _result;
+
+            Assert.That(json.Data.ToString(), Is.EqualTo("{ alive = True }"));
+        }
+
+    }
+}
